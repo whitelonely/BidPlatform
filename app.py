27ab1922c84683107fef9bdeec7dc7ec65cb8c90
whitelonely <JNwhitelonely@gmail.com>
2026-09-14 11:48:31 +0800

    Fix Bugs

--- app.py
+++ app.py
@@ -1449,4 +1449,4 @@ async def register(item:RegisterItem):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
